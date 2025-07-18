08492882dc8b46f1d492d60882ae368c646787a7
Bofa Mpama <mpamabofa@gmail.com>
2025-07-18 23:01:25 +0100

    working on team section

--- components.js
+++ components.js
@@ -35,3 +35,60 @@ class TBox extends HTMLElement{
     }
 }
 customElements.define("t-box", TBox);
+
+
+class TTeamBox extends HTMLElement{
+    connectedCallback(){
+        const imageUrl = this.getAttribute("imageUrl");
+        const name = this.getAttribute("name");
+        const role = this.getAttribute("role");
+
+        //icons urls
+        const facebookUrl = this.getAttribute("facebookUrl") || "#";
+        const twitterUrl = this.getAttribute("twitterUrl") || "#";
+        const linkedinUrl = this.getAttribute("linkedinUrl") || "#";
+        const githubUrl = this.getAttribute("githubUrl") || "#";
+
+        // Static icon paths
+        const facebookIcon = "resources/icons/icons8-facebook-64.png";
+        const twitterIcon = "resources/icons/icons8-x-64.png";
+        const linkedinIcon = "resources/icons/icons8-linkedin-64.png";
+        const githubIcon = "resources/icons/icons8-github-64.png";
+       
+
+        this.innerHTML=`
+        <div class="img">
+            <img src="${imageUrl}" class="border-radius-2 s-300px">
+            <div class="column center">
+                <h2>${name}</h2>
+                <p>${role}</p>
+                <div class="social-media-icons flex">
+                    <a href="${facebookUrl}" class="icon-style-2"><img src="${facebookIcon}"></a>
+                    <a href="${twitterUrl}" class="icon-style-2"><img src="${twitterIcon}"></a>
+                    <a href="${linkedinUrl}" class="icon-style-2"><img src="${linkedinIcon}"></a>
+                    <a href="${githubUrl}" class="icon-style-2"><img src="${githubIcon}"></a>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
+customElements.define("t-team-box", TTeamBox);
+
+fetch('team.json')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(person => {
+            const box = document.createElement('t-team-box');
+            box.setAttribute('imageUrl', person.imageUrl);
+            box.setAttribute('name', person.name);
+            box.setAttribute('role', person.role);
+            box.setAttribute('facebookUrl', person.facebookUrl);
+            box.setAttribute('twitterUrl', person.twitterUrl);
+            box.setAttribute('linkedinUrl', person.linkedinUrl);
+            box.setAttribute('githubUrl', person.githubUrl);
+            document.getElementById('t-team-section').appendChild(box);
+        });
+    });
+
+
